6a7156ebfbf7c53bf800417d9860fdfb7ec0d33b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-20 09:43:00 +0000

    Initializing graph client

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using docx_graph_pdf;
+using docx_graph_pdf.Helpers;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
 
 var config = new ConfigurationBuilder()
     .AddEnvironmentVariables()
@@ -8,4 +11,29 @@ var config = new ConfigurationBuilder()
 
 var settings = config.GetRequiredSection("DocxGraphPdf").Get<DocxGraphPdfOptions>();
 
+var client = GetAuthenticatedGraphClient(settings);
+
 Console.WriteLine("Hello, " + settings.RedirectUri);
+
+GraphServiceClient GetAuthenticatedGraphClient(DocxGraphPdfOptions options)
+{
+    var authenticationProvider = CreateAuthorizationProvider(options);
+    var graphClient = new GraphServiceClient(authenticationProvider);
+    return graphClient;
+}
+
+IAuthenticationProvider CreateAuthorizationProvider(DocxGraphPdfOptions options)
+{
+    var scopes = new List<string>()
+    {
+        "https://graph.microsoft.com/.default"
+    };
+    var authority = $"https://login.microsoftonline.com/{options.TenantID}/v2.0";
+    var cca = ConfidentialClientApplicationBuilder.Create(options.ApplicationID)
+        .WithAuthority(authority)
+        .WithRedirectUri(options.RedirectUri)
+        .WithClientSecret(options.ApplicationSecret)
+        .Build();
+    
+    return new MsalAuthenticationProvider(cca, scopes.ToArray());
+}
